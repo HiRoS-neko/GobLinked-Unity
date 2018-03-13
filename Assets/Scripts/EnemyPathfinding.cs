5e0588ad943e81941492cc94ab55529778b1cb31
ManPumpkin <32469672+ManPumpkin@users.noreply.github.com>
2018-03-13 13:54:42 -0400

    Enemy Grid Jump
    -Enemies might jump grid squares now, but will do so very quickly.

--- Assets/Scripts/EnemyPathfinding.cs
+++ Assets/Scripts/EnemyPathfinding.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 
 public class EnemyPathfinding : MonoBehaviour
 {
-    private bool[,] movableTiles;                //
-    public float visibleRange;                   //
-    private IEnumerator goHitArray;              //
-    private Collider2D[] hitObjects;             //
-    public bool search = false;                  //
-    public Rigidbody2D body;                     //
-    [Range(0, 100)] public float speed;          //
+    private bool[,] movableTiles;      
+    
+    [Tooltip("")]
+    public float visibleRange;       
+    
+    private IEnumerator goHitArray;  
+    
+    private Collider2D[] hitObjects; 
+    
+    public bool search = false;     
+    
+    public Rigidbody2D body;     
+    
+    [Range(0, 100)]
+    [Tooltip("The speed at which the enemy tracks the player")] 
+    public float speed;          
 
     void Start()
     {
@@ -25,7 +34,34 @@ public class EnemyPathfinding : MonoBehaviour
             Debug.Log("FOR loop");
             if (hitObjects[i].tag == "Goblin")
             {
-                body.velocity = (((Vector2)hitObjects[i].transform.position - body.position)).normalized*speed*Time.fixedDeltaTime;
+                //body.velocity = (((Vector2)hitObjects[i].transform.position - body.position)).normalized*speed*Time.fixedDeltaTime;
+                if (hitObjects[i].Distance(body.GetComponentInParent<Collider2D>()).distance <= visibleRange)
+                {
+                    //Check for Sector 1 (TL). (-x, +y)
+                    if (0 < (Vector2.SignedAngle(body.position, hitObjects[i].transform.position)) &&
+                        (Vector2.SignedAngle(body.position, hitObjects[i].transform.position)) < 90) 
+                    {
+                        body.transform.position = (Vector2) body.transform.position + new Vector2(-32, 32);
+                    }
+                    //Check for Sector 2 (BL). (-x, -y)
+                    if (90 < (Vector2.SignedAngle(body.position, hitObjects[i].transform.position)) &&
+                        (Vector2.SignedAngle(body.position, hitObjects[i].transform.position)) < 180) 
+                    {
+                        body.transform.position = (Vector2) body.transform.position + new Vector2(-32, -32);
+                    }
+                    //Check for Sector 3 (BR). (+x, -y)
+                    if (-180 < (Vector2.SignedAngle(body.position, hitObjects[i].transform.position)) &&
+                        (Vector2.SignedAngle(body.position, hitObjects[i].transform.position)) < -90) 
+                    {
+                        body.transform.position = (Vector2) body.transform.position + new Vector2(32, -32);
+                    }
+                    //Check for Sector 4 (TR). (+x, +y)
+                    if (90 < (Vector2.SignedAngle(body.position, hitObjects[i].transform.position)) &&
+                        (Vector2.SignedAngle(body.position, hitObjects[i].transform.position)) < 180) 
+                    {
+                        body.transform.position = (Vector2) body.transform.position + new Vector2(32, 32);
+                    }
+                }
             }
         }
     }
