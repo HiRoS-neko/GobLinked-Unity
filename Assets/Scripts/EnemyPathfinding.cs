b62f266269d0c2713ac8d982468a86a2af86641e
HiRoS <hiros.neko@gmail.com>
2018-03-20 16:15:39 -0400

    Remodified Dylan's changes as they were lost

--- Assets/Scripts/EnemyPathfinding.cs
+++ Assets/Scripts/EnemyPathfinding.cs
@@ -3,24 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnemyPathfinding : MonoBehaviour
+
 {
-    private bool[,] movableTiles;      
-    
-    [Tooltip("")]
-    public float visibleRange;       
-    
-    private IEnumerator goHitArray;  
-    
-    private Collider2D[] hitObjects; 
-    
-    public bool search = false;     
-    
-    public Rigidbody2D body;     
-    
-    [Range(0, 100)]
-    [Tooltip("The speed at which the enemy tracks the player")] 
-    public float speed;          
+    private bool[,] movableTiles;
+
+    [Tooltip("")] public float visibleRange;
+
+    private IEnumerator goHitArray;
+
+    private Collider2D[] hitObjects;
+
+    public bool search = false;
+
+    public Rigidbody2D body;
+
+    [Range(0, 100)] [Tooltip("The speed at which the enemy tracks the player")]
+    public float speed;
+
 
     void Start()
     {
@@ -34,10 +35,12 @@ public class EnemyPathfinding : MonoBehaviour
             Debug.Log("FOR loop");
             if (hitObjects[i].tag == "Goblin")
             {
-                body.velocity = (((Vector2)hitObjects[i].transform.position - body.position)).normalized*speed*Time.fixedDeltaTime; //Moves the enemy towards the goal been
+                body.velocity = (((Vector2) hitObjects[i].transform.position - body.position)).normalized * speed *
+                                Time.fixedDeltaTime; //Moves the enemy towards the goal been
             }
         }
     }
+
 
     private IEnumerator checkHitArray(float waitTime)
     {
@@ -45,7 +48,8 @@ public class EnemyPathfinding : MonoBehaviour
 
         while (true)
         {
-            hitObjects = Physics2D.OverlapCircleAll(transform.position, visibleRange); //Check for any goblins within range
+            hitObjects =
+                Physics2D.OverlapCircleAll(transform.position, visibleRange); //Check for any goblins within range
             yield return new WaitForSeconds(waitTime); //Wait for next poll time
         }
     }
