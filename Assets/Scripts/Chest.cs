41715ad476ccfd3886771fe2cb934997844d5a45
HiRoS Neko <hiros.neko@gmail.com>
2018-04-11 12:07:09 -0400

    Added Chest Script and Level Change Script. Made Dialogue a Prefab

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Chest : MonoBehaviour
+{
+    [SerializeField] private List<Item> _items;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        foreach (var item in _items)
+        {
+            Instantiate(item, transform.position, Quaternion.identity);
+        }
+
+        _items = new List<Item>();
+    }
+}
---
+++ Assets/Scripts/LevelChange.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider2D))]
+public class LevelChange : MonoBehaviour
+{
+    [SerializeField] private string _level;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(_level);
+    }
+}
