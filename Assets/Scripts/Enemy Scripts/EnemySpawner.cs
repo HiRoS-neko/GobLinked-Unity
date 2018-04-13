1068a443ee7fce8577acd94a9d762f54894bb2e7
HiRoS Neko <hiros.neko@gmail.com>
2018-04-13 15:45:57 -0400

    fix Spawner

--- Assets/Scripts/Enemy Scripts/EnemySpawner.cs
+++ Assets/Scripts/Enemy Scripts/EnemySpawner.cs
@@ -2,34 +2,33 @@
 
 public class EnemySpawner : MonoBehaviour
 {
-    [Tooltip("Prefabs of enemies")]
-    public GameObject[] enemiesToSpawn;
+    [Tooltip("Prefabs of enemies")] public GameObject[] enemiesToSpawn;
 
     [Tooltip("The weight of each prefab to actually spawn")]
     public int[] enemyWeights;
-    
-    [Tooltip("Time between respawns")] 
-    public int spawnTimer;
 
-    private int sumSofar = 0;    //Sum for the random ranges
-    
-    private float currentTimer;    //Current time elapsed
-    
-    private int check;    //Check number for the weights
-    
+    [Tooltip("Time between respawns")] public int spawnTimer;
+
+    private int sumSofar = 0; //Sum for the random ranges
+
+    private float currentTimer; //Current time elapsed
+
+    private int check; //Check number for the weights
+
     private void Update()
     {
-        if (currentTimer >= spawnTimer)    //Checking if it is time to spawn
+        currentTimer += Time.deltaTime;
+        if (currentTimer >= spawnTimer) //Checking if it is time to spawn
         {
-            check = Random.Range(0, 1);    //Random chekc number
-            for (int i = 0; i < enemiesToSpawn.Length; i++)    //Checking every spawnable prefab
+            check = Random.Range(0, 100); //Random chekc number
+            for (int i = 0; i < enemiesToSpawn.Length; i++) //Checking every spawnable prefab
             {
-               // if (check == 0)    //Checking if the digit falls inside the range of a prefab
-               // {
-               //     Instantiate(enemiesToSpawn[i]);    
-               // }
+                // if (check == 0)    //Checking if the digit falls inside the range of a prefab
+                // {
+                //     Instantiate(enemiesToSpawn[i]);    
+                // }
                 sumSofar += enemyWeights[i];
-                if(check <= sumSofar)
+                if (check <= sumSofar)
                 {
                     Instantiate(enemiesToSpawn[i]);
                 }
